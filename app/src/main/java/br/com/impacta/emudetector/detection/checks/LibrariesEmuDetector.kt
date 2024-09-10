daebffc0fcb891eb86253341bcf8f25347c636d4
Richard Araujo <richard_sep10@hotmail.com>
2024-09-09 23:49:01 -0300

    Removendo librarie usada em devices fisicos

--- app/src/main/java/br/com/impacta/emudetector/detection/checks/LibrariesEmuDetector.kt
+++ app/src/main/java/br/com/impacta/emudetector/detection/checks/LibrariesEmuDetector.kt
@@ -7,7 +7,7 @@ class LibrariesEmuDetector : EmuDetectorStrategy {
     override fun getScoreWeight() = 30
     override fun detect(): Boolean {
         val emuLibraries = listOf(
-            "/system/lib/libhoudini.so", "/system/lib/libc_malloc_debug.so",
+            "/system/lib/libhoudini.so",
             "/system/lib/libdroid4x.so", "/system/lib/libbluestacks.so",
             "/system/lib/libnox.so", "/system/lib/libgenymotion.so",
             "/system/lib/libandyroid.so", "/system/lib/libemulator.so",
